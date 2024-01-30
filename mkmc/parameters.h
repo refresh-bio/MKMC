c941e9acf200d6ecc4995dd6ba8519ec7f608193
maciejdlugosz <maciej.dlugosz@polsl.pl>
2024-01-30 10:28:59 +0100

    Korekta domyślnej dostępnej pamięci.

--- mkmc/parameters.h
+++ mkmc/parameters.h
@@ -14,7 +14,7 @@ struct MKMCParams
 	std::vector<std::string> toolsOutputFiles;
 	uint32_t nThreads = std::thread::hardware_concurrency();
 	uint32_t nKMCWorkers = 8;
-	uint32_t maxRamGB = 12;
+	uint32_t maxRamGB = 16;
 	std::string outputFile;
 	bool dumpToFile = true;
 	uint64_t dumpStepSize = static_cast<uint64_t>(1E9);
