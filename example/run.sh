228d083275bcd8e93bd0ac5a8585f99470fe29f7
marekkokot <marek.koki@gmail.com>
2024-02-08 15:43:35 +0100

    example to have an option to for simple checking if it works after compile, kind of smoke test I guess

---
+++ example/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p tmp
+../bin/mkmc -k25 -fq -thr1 @files.txt present-in-all.txt tmp
+..bin/mkmc -k25 -fq -thr0.5 @files.txt present-in-at-least-half-files.txt tmp
+..bin/mkmc -k25 -fq -thr0 @files.txt present-in-any.txt tmp
