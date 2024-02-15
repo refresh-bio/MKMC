96e995eae1cacfa3e9a388a8997006327a0689be
maciejdlugosz <maciej.dlugosz@polsl.pl>
2024-02-15 18:06:47 +0100

    Aktualizcja przykładu do nowego wiersza poleceń.

--- example/run.sh
+++ example/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p tmp
-../bin/mkmc -k25 -fq -thr1 @files.txt present-in-all.txt tmp
-../bin/mkmc -k25 -fq -thr0.5 @files.txt present-in-at-least-half-files.txt tmp
-../bin/mkmc -k25 -fq -thr0 @files.txt present-in-any.txt tmp
+../bin/mkmc -k25 -fq -thr_rat1 @files.txt present-in-all.txt tmp
+../bin/mkmc -k25 -fq -thr_rat0.5 @files.txt present-in-at-least-half-files.txt tmp
+../bin/mkmc -k25 -fq -thr_rat0 @files.txt present-in-any.txt tmp
