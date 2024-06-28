b5d1c17f88ef2a51e548862f4326bdbc695beda8
marekkokot <marek.koki@gmail.com>
2024-06-28 13:21:19 +0200

    make examples consistent with readme

--- example/run.sh
+++ example/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p tmp
-../bin/mkmc -k25 -fq -thr_rat1 @files.txt present-in-all.txt tmp
-../bin/mkmc -k25 -fq -thr_rat0.5 @files.txt present-in-at-least-half-files.txt tmp
-../bin/mkmc -k25 -fq -thr_rat0 @files.txt present-in-any.txt tmp
+../bin/mkmc -k25 -f fq -thr_rat 1 files.txt present-in-all.txt tmp
+../bin/mkmc -k 25 -f fq -thr_rat 0.5 files.txt present-in-at-least-half-files.txt tmp
+../bin/mkmc -k 25 -f fq -thr_rat 0 files.txt present-in-any.txt tmp
