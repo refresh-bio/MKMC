04851a24b81340d2bd87d18a583e38d5ac3d5f9e
Maciej Długosz <maciej.dlugosz@polsl.pl>
2026-02-06 21:31:18 +0100

    Update PCA to new version.

--- 3rd_party/refresh/statistics/lib/statistics_pca.h
+++ 3rd_party/refresh/statistics/lib/statistics_pca.h
@@ -2,11 +2,13 @@
 #define _STATISTICS_PCA_H
 
 #define EIGEN_NO_DEPRECATED_WARNING
-#include "umappp/Umap.hpp"
-
+#include <Eigen/Dense>
 #include <vector>
 #include <iterator>
 #include <cinttypes>
+#include <numeric>
+#include <algorithm>
+#include <cassert>
 
 namespace refresh
 {
@@ -17,16 +19,15 @@ namespace refresh
 		enum class computation_mode_t { covariance, svd };
 
 	protected:
-		enum class input_mode_t {unknown, feature_oriented, object_oriented};
+		enum class input_mode_t { unknown, feature_oriented, object_oriented };
 
 		using entry_t = std::vector<VALUE_T>;
 
 		std::vector<entry_t> input_data;
 		std::vector<entry_t> ret_data;
-		
+
 		std::vector<VALUE_T> explained_variance;
 		std::vector<VALUE_T> explained_variance_ratio;
-
 
 		input_mode_t input_mode = input_mode_t::unknown;
 		size_t input_vector_size{};
@@ -49,11 +50,10 @@ namespace refresh
 			else if (input_mode != requested_mode)
 				return false;
 
-			if (std::distance(first, last) != input_vector_size)
+			if (static_cast<size_t>(std::distance(first, last)) != input_vector_size)
 				return false;
 
 			input_data.emplace_back(first, last);
-
 			return true;
 		}
 
@@ -61,160 +61,108 @@ namespace refresh
 		{
 			vec_clear(input_data);
 			vec_clear(ret_data);
-
+			vec_clear(explained_variance);
+			vec_clear(explained_variance_ratio);
 			input_mode = input_mode_t::unknown;
 			input_vector_size = 0;
 		}
 
-		bool run_covariance(size_t no_dimensions = 2)
+		// Pomocnicza funkcja do budowania macierzy Eigen z input_data
+		Eigen::MatrixXd build_matrix() const
 		{
-			if (input_mode == input_mode_t::unknown)
-				return false;
-
-			size_t k = 0;
 			size_t no_objects = 0;
 			size_t no_features = 0;
-
 			Eigen::MatrixXd X;
 
-			switch (input_mode)
+			if (input_mode == input_mode_t::object_oriented)
 			{
-			case input_mode_t::object_oriented:
 				no_objects = input_data.size();
 				no_features = input_data.front().size();
-
 				X.resize(no_objects, no_features);
-
 				for (size_t i = 0; i < no_objects; ++i)
 					for (size_t j = 0; j < no_features; ++j)
-						X(i, j) = input_data[i][j];
-
-				break;
-			case input_mode_t::feature_oriented:
+						X(i, j) = static_cast<double>(input_data[i][j]);
+			}
+			else if (input_mode == input_mode_t::feature_oriented)
+			{
 				no_objects = input_data.front().size();
 				no_features = input_data.size();
-
 				X.resize(no_objects, no_features);
-
 				for (size_t i = 0; i < no_objects; ++i)
 					for (size_t j = 0; j < no_features; ++j)
-						X(i, j) = input_data[j][i];
-
-				break;
-			default:
-				assert(0);						// Never should be here
+						X(i, j) = static_cast<double>(input_data[j][i]);
 			}
+			return X;
+		}
 
-			// PCA calculations
+		bool run_covariance(size_t no_dimensions = 2)
+		{
+			if (input_mode == input_mode_t::unknown || input_data.empty())
+				return false;
+
+			Eigen::MatrixXd X = build_matrix();
+			size_t no_objects = X.rows();
+
 			auto centered = X.rowwise() - X.colwise().mean();
 
-			auto covariance = (centered.adjoint() * centered) / double(X.rows() - 1);
+			auto covariance = (centered.adjoint() * centered) / double(no_objects - 1);
 
 			Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> eig(covariance);
-
 			if (eig.info() != Eigen::Success)
 				return false;
 
 			Eigen::VectorXd eigenvalues = eig.eigenvalues().reverse();
-			Eigen::MatrixXd eigenvectors = eig.eigenvectors().rowwise().reverse();
+			Eigen::MatrixXd eigenvectors = eig.eigenvectors().colwise().reverse();
 
 			explained_variance.resize(eigenvalues.size());
 			for (int i = 0; i < eigenvalues.size(); ++i)
-				explained_variance[i] = eigenvalues(i);
+				explained_variance[i] = static_cast<VALUE_T>(eigenvalues(i));
 
-			double total = std::accumulate(explained_variance.begin(), explained_variance.end(), VALUE_T(0));
+			double total = std::accumulate(explained_variance.begin(), explained_variance.end(), 0.0);
 			explained_variance_ratio.resize(explained_variance.size());
 			for (size_t i = 0; i < explained_variance.size(); ++i)
-				explained_variance_ratio[i] = explained_variance[i] / total;
+				explained_variance_ratio[i] = static_cast<VALUE_T>(double(explained_variance[i]) / total);
 
-			auto principal_components = eigenvectors.leftCols(no_dimensions);
-
+			auto principal_components = eigenvectors.leftCols(std::min(no_dimensions, (size_t)eigenvectors.cols()));
 			auto transformed = centered * principal_components;
 
-			// Convert results to vector of vectors
-			ret_data.clear();
-			ret_data.resize(no_objects, entry_t(no_dimensions));
-
-			k = 0;
+			ret_data.assign(no_objects, entry_t(transformed.cols()));
 			for (size_t i = 0; i < no_objects; ++i)
-			{
-				ret_data[i].resize(no_dimensions);
-				for (size_t j = 0; j < no_dimensions; ++j)
-					ret_data[i][j] = transformed(i, j);
-			}
+				for (size_t j = 0; j < (size_t)transformed.cols(); ++j)
+					ret_data[i][j] = static_cast<VALUE_T>(transformed(i, j));
 
 			return true;
 		}
 
 		bool run_svd(size_t no_dimensions = 2)
 		{
-			if (input_mode == input_mode_t::unknown)
+			if (input_mode == input_mode_t::unknown || input_data.empty())
 				return false;
 
-			size_t k = 0;
-			size_t no_objects = 0;
-			size_t no_features = 0;
+			Eigen::MatrixXd X = build_matrix();
+			size_t no_objects = X.rows();
 
-			Eigen::MatrixXd X;
-
-			switch (input_mode)
-			{
-			case input_mode_t::object_oriented:
-				no_objects = input_data.size();
-				no_features = input_data.front().size();
-
-				X.resize(no_objects, no_features);
-
-				for (size_t i = 0; i < no_objects; ++i)
-					for (size_t j = 0; j < no_features; ++j)
-						X(i, j) = input_data[i][j];
-
-				break;
-			case input_mode_t::feature_oriented:
-				no_objects = input_data.front().size();
-				no_features = input_data.size();
-
-				X.resize(no_objects, no_features);
-
-				for (size_t i = 0; i < no_objects; ++i)
-					for (size_t j = 0; j < no_features; ++j)
-						X(i, j) = input_data[j][i];
-
-				break;
-			default:
-				assert(0);						// Never should be here
-			}
-
-			// PCA calculations
 			auto centered = X.rowwise() - X.colwise().mean();
 
-			Eigen::JacobiSVD<Eigen::MatrixXd> svd(X, Eigen::ComputeThinU | Eigen::ComputeThinV);
+			Eigen::JacobiSVD<Eigen::MatrixXd> svd(centered, Eigen::ComputeThinU | Eigen::ComputeThinV);
 
-			Eigen::MatrixXd principal_components = svd.matrixV().leftCols(no_dimensions);
-			
-			auto transformed = centered * principal_components;			
+			Eigen::MatrixXd principal_components = svd.matrixV().leftCols(std::min(no_dimensions, (size_t)svd.matrixV().cols()));
+			auto transformed = centered * principal_components;
 
 			Eigen::VectorXd s = svd.singularValues();
 			explained_variance.resize(s.size());
 			for (int i = 0; i < s.size(); ++i)
-				explained_variance[i] = (s(i) * s(i)) / (no_objects - 1);
+				explained_variance[i] = static_cast<VALUE_T>((s(i) * s(i)) / (double(no_objects) - 1.0));
 
-			double total = std::accumulate(explained_variance.begin(), explained_variance.end(), VALUE_T(0));
+			double total = std::accumulate(explained_variance.begin(), explained_variance.end(), 0.0);
 			explained_variance_ratio.resize(explained_variance.size());
 			for (size_t i = 0; i < explained_variance.size(); ++i)
-				explained_variance_ratio[i] = explained_variance[i] / total;
+				explained_variance_ratio[i] = static_cast<VALUE_T>(double(explained_variance[i]) / total);
 
-			// Convert results to vector of vectors
-			ret_data.clear();
-			ret_data.resize(no_objects, entry_t(no_dimensions));
-
-			k = 0;
+			ret_data.assign(no_objects, entry_t(transformed.cols()));
 			for (size_t i = 0; i < no_objects; ++i)
-			{
-				ret_data[i].resize(no_dimensions);
-				for (size_t j = 0; j < no_dimensions; ++j)
-					ret_data[i][j] = transformed(i, j);
-			}
+				for (size_t j = 0; j < (size_t)transformed.cols(); ++j)
+					ret_data[i][j] = static_cast<VALUE_T>(transformed(i, j));
 
 			return true;
 		}
@@ -222,22 +170,13 @@ namespace refresh
 	public:
 		pca() = default;
 
-		void reset()
-		{
-			clear();
-		}
+		void reset() { clear(); }
 
 		template<typename Iter>
-		bool add_object(Iter first, Iter last)
-		{
-			return try_add(input_mode_t::object_oriented, first, last);
-		}
+		bool add_object(Iter first, Iter last) { return try_add(input_mode_t::object_oriented, first, last); }
 
 		template<typename Iter>
-		bool add_feature(Iter first, Iter last)
-		{
-			return try_add(input_mode_t::feature_oriented, first, last);
-		}
+		bool add_feature(Iter first, Iter last) { return try_add(input_mode_t::feature_oriented, first, last); }
 
 		bool run(size_t no_dimensions = 2, computation_mode_t computation_mode = computation_mode_t::svd)
 		{
@@ -248,47 +187,27 @@ namespace refresh
 			return false;
 		}
 
-
-		const std::vector<std::vector<VALUE_T>>& result() const noexcept
-		{
-			return ret_data;
-		}
-
-		const std::vector<VALUE_T>& get_explained_variance() const noexcept 
-		{
-			return explained_variance;
-		}
-
-		const std::vector<VALUE_T>& get_explained_variance_ratio() const noexcept 
-		{
-			return explained_variance_ratio;
-		}
+		const std::vector<std::vector<VALUE_T>>& result() const noexcept { return ret_data; }
+		const std::vector<VALUE_T>& get_explained_variance() const noexcept { return explained_variance; }
+		const std::vector<VALUE_T>& get_explained_variance_ratio() const noexcept { return explained_variance_ratio; }
 	};
-
 
 	template<typename VALUE_T>
 	class pca_parallel_add : public pca<VALUE_T>
 	{
-		size_t no_features;
-
+		size_t no_features_val;
 	public:
-		pca_parallel_add(size_t no_objects, size_t no_features) :
-			no_features(no_features)
+		pca_parallel_add(size_t no_objects, size_t no_features) : no_features_val(no_features)
 		{
-			pca<VALUE_T>::input_vector_size = no_objects;
-			pca<VALUE_T>::input_mode = pca<VALUE_T>::input_mode_t::feature_oriented;
-			pca<VALUE_T>::input_data.resize(no_features, typename pca<VALUE_T>::entry_t(no_objects));
+			this->input_vector_size = no_objects;
+			this->input_mode = pca<VALUE_T>::input_mode_t::feature_oriented;
+			this->input_data.assign(no_features, typename pca<VALUE_T>::entry_t(no_objects));
 		}
-
-		template<typename Iter>
-		bool add_object(Iter first, Iter last) = delete;
-
-		template<typename Iter>
-		bool add_feature(Iter first, Iter last) = delete;
 
 		bool add(size_t object_idx, size_t feature_idx, const VALUE_T value)
 		{
-			pca<VALUE_T>::input_data[feature_idx][object_idx] = value;
+			if (feature_idx >= this->input_data.size() || object_idx >= this->input_vector_size) return false;
+			this->input_data[feature_idx][object_idx] = value;
 			return true;
 		}
 	};
